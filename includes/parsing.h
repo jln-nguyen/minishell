750550c49118de9c99be510fa1b4a2d9b9aeae93
Julien Nguyen <junguyen@made-f0Ar9s5.clusters.42paris.fr>
2024-10-29 21:25:36 +0100

    commencement

--- includes/parsing.h
+++ includes/parsing.h
@@ -6,11 +6,36 @@
 /*   By: junguyen <junguyen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 11:11:39 by junguyen          #+#    #+#             */
-/*   Updated: 2024/10/29 11:12:30 by junguyen         ###   ########.fr       */
+/*   Updated: 2024/10/29 18:56:49 by junguyen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSING_H
 # define PARSING_H
 
-#endif PARSING_H
+# include "libft.h"
+
+typedef enum e_enum_type
+{
+	TOKEN_STR,
+	TOKEN_PIPE,
+	TOKEN_REDIR_IN,
+	TOKEN_REDIR_OUT,
+	TOKEN_REDIR_APPEND,
+	TOKEN_REDIR_HEREDOC,
+	TOKEN_ENV_VAR,
+}	t_enum_type;
+
+typedef struct s_token
+{
+	t_enum_type		type;
+	char			*value;
+	struct s_token	*next;	
+}	t_token;
+
+int	check_syntax(char *str);
+
+t_token	*new_tok(int type, char *str);
+void	ft_tokadd_back(t_token **lst, t_token *new);
+
+#endif 
