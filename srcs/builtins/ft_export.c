b166b722beaeb326de6dc951a92b484f28dfc7b4
Brendan Victoire <bvictoir@made-f0Br2s1.clusters.42paris.fr>
2024-12-03 11:01:22 +0100

    comment main

--- srcs/builtins/ft_export.c
+++ srcs/builtins/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: bvictoir <bvictoir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 14:54:09 by bvictoir          #+#    #+#             */
-/*   Updated: 2024/12/03 10:15:19 by bvictoir         ###   ########.fr       */
+/*   Updated: 2024/12/03 10:43:02 by bvictoir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,48 +84,48 @@ void	ft_export(t_env **env, t_token *tok)
 	}
 }
 
-void	add_env(t_env **env, char *str)
-{
-	t_env	*new;
-	char	*equal;
+// void	add_env(t_env **env, char *str)
+// {
+// 	t_env	*new;
+// 	char	*equal;
 
-	equal = ft_strchr(str, '=');
-	if (!equal)
-		return ;
-	new = malloc(sizeof(t_env));
-	if (!new)
-		return ;
-	new->key = ft_substr(str, 0, equal - str);
-	new->value = ft_strdup(equal + 1);
-	new->next = *env;
-	*env = new;
-}
-int main(int ac, char **av, char **env)
-{
-	(void)ac, (void)av;
-	t_env *env_list;
-	t_token *tok;
+// 	equal = ft_strchr(str, '=');
+// 	if (!equal)
+// 		return ;
+// 	new = malloc(sizeof(t_env));
+// 	if (!new)
+// 		return ;
+// 	new->key = ft_substr(str, 0, equal - str);
+// 	new->value = ft_strdup(equal + 1);
+// 	new->next = *env;
+// 	*env = new;
+// }
+// int main(int ac, char **av, char **env)
+// {
+// 	(void)ac, (void)av;
+// 	t_env *env_list;
+// 	t_token *tok;
 
-	tok = malloc(sizeof(t_token));
-	tok->value = "export";
-	tok->next = NULL;
-	tok->next = malloc(sizeof(t_token));
-	tok->next->value = "d=";
-	tok->next->next = NULL;
-	tok->next->next = malloc(sizeof(t_token));
-	tok->next->next->value = "e=42";
-	tok->next->next->next = NULL;
+// 	tok = malloc(sizeof(t_token));
+// 	tok->value = "export";
+// 	tok->next = NULL;
+// 	tok->next = malloc(sizeof(t_token));
+// 	tok->next->value = "d=";
+// 	tok->next->next = NULL;
+// 	tok->next->next = malloc(sizeof(t_token));
+// 	tok->next->next->value = "e=42";
+// 	tok->next->next->next = NULL;
 
-	env_list = NULL;
-	while (*env)
-	{
-		add_env(&env_list, *env);
-		env++;
-	}
-	ft_export(&env_list, tok);
-	tok->next = NULL;
-	ft_export(&env_list, tok);
-	// ft_env(&env_list);
+// 	env_list = NULL;
+// 	while (*env)
+// 	{
+// 		add_env(&env_list, *env);
+// 		env++;
+// 	}
+// 	ft_export(&env_list, tok);
+// 	tok->next = NULL;
+// 	ft_export(&env_list, tok);
+// 	// ft_env(&env_list);
 
-	return (0);
-}
+// 	return (0);
+// }
