6870c1cf1ecd4f9b65aaef387bc507aa79d3f075
BrendanVKM <62537693+BrendanVKM@users.noreply.github.com>
2024-12-20 15:34:32 +0100

    pipe

--- srcs/exec/ft_pipe.c
+++ srcs/exec/ft_pipe.c
@@ -6,13 +6,24 @@
 /*   By: bvictoir <bvictoir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 10:46:44 by bvictoir          #+#    #+#             */
-/*   Updated: 2024/12/20 13:55:30 by bvictoir         ###   ########.fr       */
+/*   Updated: 2024/12/20 15:01:14 by bvictoir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	exec_pipe_left(t_ast_node **ast, t_env **env, int *pipefd)
+static void	cleanup_child(t_ast_node **ast, t_env **env, int *pipefd)
+{
+	close(pipefd[0]);
+	close(pipefd[1]);
+	if (ast && *ast)
+		ft_free_ast(ast);
+	if (env && *env)
+		ft_free_env(env);
+	exit(EXIT_FAILURE);
+}
+
+static int	handle_left_pipe(t_ast_node **ast, t_env **env, int *pipefd)
 {
 	pid_t	pid;
 
@@ -25,7 +36,7 @@ static int	exec_pipe_left(t_ast_node **ast, t_env **env, int *pipefd)
 		if (dup2(pipefd[1], STDOUT_FILENO) == -1)
 		{
 			perror("dup2");
-			exit(EXIT_FAILURE);
+			cleanup_child(ast, env, pipefd);
 		}
 		close(pipefd[1]);
 		ft_exec(&((*ast)->left), env);
@@ -36,7 +47,7 @@ static int	exec_pipe_left(t_ast_node **ast, t_env **env, int *pipefd)
 	return (pid);
 }
 
-static int	exec_pipe_right(t_ast_node **ast, t_env **env, int *pipefd)
+static int	handle_right_pipe(t_ast_node **ast, t_env **env, int *pipefd)
 {
 	pid_t	pid;
 
@@ -49,13 +60,10 @@ static int	exec_pipe_right(t_ast_node **ast, t_env **env, int *pipefd)
 		if (dup2(pipefd[0], STDIN_FILENO) == -1)
 		{
 			perror("dup2");
-			exit(EXIT_FAILURE);
+			cleanup_child(ast, env, pipefd);
 		}
 		close(pipefd[0]);
-		if ((*ast)->right->type == TOKEN_PIPE)
-			exec_pipe(&(*ast)->right, env);
-		else
-			ft_exec(&((*ast)->right), env);
+		ft_exec(&((*ast)->right), env);
 		ft_free_ast(ast);
 		ft_free_env(env);
 		exit(EXIT_SUCCESS);
@@ -80,10 +88,10 @@ void	exec_pipe(t_ast_node **ast, t_env **env)
 
 	if (!init_pipe(pipefd, ast))
 		return ;
-	left_pid = exec_pipe_left(ast, env, pipefd);
+	left_pid = handle_left_pipe(ast, env, pipefd);
 	if (left_pid == -1)
 		return ((void)(close(pipefd[0]), close(pipefd[1])));
-	right_pid = exec_pipe_right(ast, env, pipefd);
+	right_pid = handle_right_pipe(ast, env, pipefd);
 	if (right_pid == -1 && left_pid > 0)
 		return ((void)(close(pipefd[0]), close(pipefd[1]), 
 			waitpid(left_pid, NULL, 0)));
@@ -93,4 +101,6 @@ void	exec_pipe(t_ast_node **ast, t_env **env)
 		waitpid(left_pid, NULL, 0);
 	if (right_pid > 0)
 		waitpid(right_pid, NULL, 0);
+	ft_free_ast(ast);
+	ft_free_env(env);
 }
