480bc1d9c6eae41889ea1fc2612f80633052b8a1
bvkm <62537693+BrendanVKM@users.noreply.github.com>
2025-02-02 13:29:57 +0100

    redir norm + dup

--- srcs/exec/ft_check_redir.c
+++ srcs/exec/ft_check_redir.c
@@ -6,7 +6,7 @@
 /*   By: bvkm <bvkm@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 17:19:48 by junguyen          #+#    #+#             */
-/*   Updated: 2025/02/02 13:23:23 by bvkm             ###   ########.fr       */
+/*   Updated: 2025/02/02 13:29:04 by bvkm             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_reset_fd(int old_fd_in, int old_fd_out, t_data *data)
 	if (old_fd_in != 0)
 	{
 		if (dup2(old_fd_in, STDIN_FILENO) == -1)
-			 (close(old_fd_in), ft_err(data, "dup2"));
+			(close(old_fd_in), ft_err(data, "dup2"));
 		close(old_fd_in);
 	}
 	if (old_fd_out != 1)
@@ -28,12 +28,12 @@ void	ft_reset_fd(int old_fd_in, int old_fd_out, t_data *data)
 	}
 }
 
-static int	ft_dup_out(int file_out, int old_fd_out, int old_fd_in, t_data *data)
+static int	ft_dup_out(int file_out, int old_out, int old_in, t_data *data)
 {
 	if (file_out > 0)
 	{
-		old_fd_out = dup(STDOUT_FILENO);
-		if (old_fd_out == -1)
+		old_out = dup(STDOUT_FILENO);
+		if (old_out == -1)
 			ft_err(data, "dup");
 		if (dup2(file_out, STDOUT_FILENO) == -1)
 			(close(file_out), ft_err(data, "dup2"));
@@ -41,10 +41,23 @@ static int	ft_dup_out(int file_out, int old_fd_out, int old_fd_in, t_data *data)
 	}
 	else if (file_out < 0)
 	{
-		ft_reset_fd(old_fd_in, old_fd_out, data);
+		ft_reset_fd(old_in, old_out, data);
 		return (-1);
 	}
-	return (old_fd_out);
+	return (old_out);
+}
+
+static void	fd_dup(int file_in, int old_fd_in, t_data *data)
+{
+	if (file_in > 0)
+	{
+		old_fd_in = dup(STDIN_FILENO);
+		if (old_fd_in == -1)
+			ft_err(data, "dup");
+		if (dup2(file_in, STDIN_FILENO) == -1)
+			ft_err(data, "dup2");
+		close(file_in);
+	}
 }
 
 void	ft_redir(t_data *data, t_ast_node **ast)
@@ -58,15 +71,7 @@ void	ft_redir(t_data *data, t_ast_node **ast)
 	old_fd_out = 1;
 	search_cmd(ast, data);
 	file_in = ft_redir_in(*ast, &data->env);
-	if (file_in > 0)
-	{
-		old_fd_in = dup(STDIN_FILENO);
-		if (old_fd_in == -1)
-			ft_err(data, "dup");
-		if (dup2(file_in, STDIN_FILENO) == -1)
-			ft_err(data, "dup2");
-		close(file_in);
-	}
+	fd_dup(file_in, old_fd_in, data);
 	if (file_in < 0)
 		return (data->exit_code = 1, ft_reset_fd(old_fd_in, old_fd_out, data));
 	file_out = ft_redir_out(*ast);
