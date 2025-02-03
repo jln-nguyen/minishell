1586ca0f5661c48c5f1d1437ecf901a1bcd8046a
Brendan Victoire <bvictoir@paul-f4Ar5s5.clusters.42paris.fr>
2025-02-03 10:34:03 +0100

    removed test printf

--- srcs/exec/ft_execve_utils.c
+++ srcs/exec/ft_execve_utils.c
@@ -6,9 +6,9 @@
 /*   By: bvictoir <bvictoir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 10:59:57 by bvictoir          #+#    #+#             */
-/*   Updated: 2025/02/03 10:31:02 by bvictoir         ###   ########.fr       */
+/*   Updated         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/* *********************************************************************** */
 
 #include "minishell.h"
 
@@ -17,10 +17,7 @@ void	ft_no_path(t_data *data, t_ast_node **ast, char **env, char *path)
 	ft_printf(STDERR_FILENO, "Minishell: %s: command not found\n",
 		(*ast)->args[0]);
 	if (path[0])
-	{
-		printf("%s\n", path);
 		free(path);
-	}
 	ft_free_tab(&env);
 	ft_free_ast(&data->ast);
 	ft_free_env(&data->env);
